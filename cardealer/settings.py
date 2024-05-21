dcd1ce169ac489fe51d12fc6b13082fd05525acf
Mb4y4 <d.sommy@gmail.com>
2024-05-21 11:50:14 +0300

    Time zone

--- cardealer/settings.py
+++ cardealer/settings.py
@@ -91,7 +91,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cardealer.wsgi:application'
+WSGI_APPLICATION = 'cardealer.wsgi.application'
 
 
 # Database
@@ -137,16 +137,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
+# LANGUAGE_CODE = 'en-us'
+
+# TIME_ZONE = 'UTC'
+
+# USE_I18N = True
+
+# USE_L10N = True
+
+# USE_TZ = False
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = False
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
