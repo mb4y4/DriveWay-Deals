8d5cd1acebb57eca30a3dbece170208d325cbe99
Mb4y4 <d.sommy@gmail.com>
2024-07-19 13:25:13 +0300

    Integrate React frontend

--- cardealer/urls.py
+++ cardealer/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import index  # Import the view
 
 
 urlpatterns = [
@@ -10,5 +11,6 @@ urlpatterns = [
     path('cars/', include('cars.urls')),
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
+    path('', index),  # Catch-all pattern for the React app
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
